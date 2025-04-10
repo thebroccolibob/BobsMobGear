b0f4c989d11dc5bfc64f95e48ee781be5e382620
thebroccolibob <thebroccolibob@gmail.com>
2025-04-09 19:42:52 -0700

    Initial Commit

---
+++ src/client/kotlin/io/github/thebroccolibob/bobsmobgear/BobsMobGearClient.kt
@@ -0,0 +1,9 @@
+package io.github.thebroccolibob.bobsmobgear
+
+import net.fabricmc.api.ClientModInitializer
+
+object BobsMobGearClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
