04a4993dc5f818c7955c60b14e89a782fed4eac7
JosiahFu <josiahfu@gmail.com>
2025-05-05 03:42:17 -0700

    Models and placeholder textures

--- src/client/kotlin/io/github/thebroccolibob/bobsmobgear/datagen/ModelGenerator.kt
+++ src/client/kotlin/io/github/thebroccolibob/bobsmobgear/datagen/ModelGenerator.kt
@@ -1,6 +1,7 @@
 package io.github.thebroccolibob.bobsmobgear.datagen
 
 import io.github.thebroccolibob.bobsmobgear.BobsMobGear
+import io.github.thebroccolibob.bobsmobgear.block.ForgeBlock
 import io.github.thebroccolibob.bobsmobgear.block.TemplateBlock
 import io.github.thebroccolibob.bobsmobgear.client.util.BlockStateVariant
 import io.github.thebroccolibob.bobsmobgear.client.util.variantYRotation
@@ -10,12 +11,110 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
 import net.minecraft.block.Block
 import net.minecraft.data.client.*
+import net.minecraft.data.client.TextureMap.getSubId
 import net.minecraft.item.Item
+import net.minecraft.util.Identifier
 import java.util.*
 
 class ModelGenerator(output: FabricDataOutput) : FabricModelProvider(output) {
+    private fun BlockStateModelGenerator.registerForge(block: Block) {
+        val textures = object {
+            private fun of(suffix: String) = ModelIds.getBlockSubModelId(block, "_$suffix")
+
+            val frontLeft = of("front_left")
+            val frontRight = of("front_right")
+            val frontLeftLit = of("front_left_lit")
+            val frontRightLit = of("front_right_lit")
+            val sideLeft = of("side_left")
+            val sideRight = of("side_right")
+            val topFrontLeft = of("top_front_left")
+            val topFrontRight = of("top_front_right")
+            val topBackLeft = of("top_back_left")
+            val topBackRight = of("top_back_right")
+            val bottomFrontLeft = of("bottom_front_left")
+            val bottomFrontRight = of("bottom_front_right")
+            val bottomBackLeft = of("bottom_back_left")
+            val bottomBackRight = of("bottom_back_right")
+            val inside = of("inside")
+            val side = of("side")
+        }
+
+        fun cube(
+            up: Identifier = textures.inside,
+            down: Identifier = textures.inside,
+            north: Identifier = textures.inside,
+            south: Identifier = textures.inside,
+            east: Identifier = textures.inside,
+            west: Identifier = textures.inside,
+        ) = TextureMap().apply {
+            put(TextureKey.PARTICLE, textures.side)
+            put(TextureKey.UP, up)
+            put(TextureKey.DOWN, down)
+            put(TextureKey.NORTH, north)
+            put(TextureKey.SOUTH, south)
+            put(TextureKey.EAST, east)
+            put(TextureKey.WEST, west)
+        }
+
+        val models = ForgeBlock.Connection.entries.associateWith { connection ->
+            listOf(true, false).associateWith { lit ->
+                if (connection == ForgeBlock.Connection.NONE)
+                    Models.ORIENTABLE.upload(block, if (lit) "_lit" else "", if (lit) TextureMap().apply {
+                        put(TextureKey.SIDE, getSubId(block, "_side"))
+                        put(TextureKey.FRONT, getSubId(block, "_front_lit"))
+                        put(TextureKey.TOP, getSubId(block, "_top"))
+                        put(TextureKey.BOTTOM, getSubId(block, "_bottom"));
+                    } else TextureMap.sideFrontTopBottom(block), modelCollector)
+                else
+                    Models.CUBE.upload(
+                        block, "_${connection.id}${if (lit) "_lit" else ""}", when (connection) {
+                            ForgeBlock.Connection.FRONT_LEFT -> cube(
+                                up = textures.topFrontLeft,
+                                down = textures.bottomFrontLeft,
+                                east = textures.sideRight,
+                                north = if (lit) textures.frontLeftLit else textures.frontLeft
+                            )
+
+                            ForgeBlock.Connection.FRONT_RIGHT -> cube(
+                                up = textures.topFrontRight,
+                                down = textures.bottomFrontRight,
+                                west = textures.sideLeft,
+                                north = if (lit) textures.frontRightLit else textures.frontRight
+                            )
+
+                            ForgeBlock.Connection.BACK_LEFT -> cube(
+                                up = textures.topBackLeft,
+                                down = textures.bottomBackLeft,
+                                east = textures.sideLeft,
+                                south = textures.sideRight
+                            )
+
+                            ForgeBlock.Connection.BACK_RIGHT -> cube(
+                                up = textures.topBackRight,
+                                down = textures.bottomBackRight,
+                                west = textures.sideRight,
+                                south = textures.sideLeft
+                            )
+
+                            else -> throw AssertionError()
+                        }, modelCollector
+                    )
+            }
+        }
+
+        registerStates(block,
+            BlockStateVariantMap.create(ForgeBlock.CONNECTION, ForgeBlock.FACING, ForgeBlock.LIT).register { connection, facing, lit ->
+                BlockStateVariant(
+                    model = models[connection]!![lit]!!,
+                    y = variantYRotation(facing),
+                )
+            }
+        )
+    }
+
     override fun generateBlockStateModels(blockStateModelGenerator: BlockStateModelGenerator) {
         blockStateModelGenerator.registerTemplate(BobsMobGearBlocks.SWORD_TEMPLATE)
+        blockStateModelGenerator.registerForge(BobsMobGearBlocks.FORGE)
     }
 
     override fun generateItemModels(itemModelGenerator: ItemModelGenerator) {
