9b38c35caa2fd933e2a6e1b564099583670c0fb8
JosiahFu <josiahfu@gmail.com>
2025-06-12 03:18:03 -0700

    Hammer special attack test

--- src/main/kotlin/io/github/thebroccolibob/bobsmobgear/item/BoneHammerItem.kt
+++ src/main/kotlin/io/github/thebroccolibob/bobsmobgear/item/BoneHammerItem.kt
@@ -1,5 +1,7 @@
 package io.github.thebroccolibob.bobsmobgear.item
 
+import io.github.thebroccolibob.bobsmobgear.registry.BobsMobGearItems
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.SwordItem
@@ -14,5 +16,12 @@ class BoneHammerItem(material: ToolMaterial, settings: Settings) : ToolItem(mate
 }), HasSpecialAttack {
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         return runSpecialAttack(user, hand, world)
+    }
+
+    override fun postHit(stack: ItemStack?, target: LivingEntity?, attacker: LivingEntity?): Boolean = true
+
+    override fun postDamageEntity(stack: ItemStack, target: LivingEntity, attacker: LivingEntity) {
+        if (BobsMobGearItems.USING_SPECIAL_ATTACK in stack)
+            target.addVelocity(0.0, 0.5, 0.0) // placeholder
     }
 }
