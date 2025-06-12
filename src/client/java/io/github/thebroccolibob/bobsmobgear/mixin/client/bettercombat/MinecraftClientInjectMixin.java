7f8367fe9da04fb53833f6d933baceba59f5bbd1
JosiahFu <josiahfu@gmail.com>
2025-06-12 11:04:52 -0700

    Change mixin to use mixinsquared

--- src/client/java/io/github/thebroccolibob/bobsmobgear/mixin/client/bettercombat/MinecraftClientInjectMixin.java
+++ src/client/java/io/github/thebroccolibob/bobsmobgear/mixin/client/bettercombat/MinecraftClientInjectMixin.java
@@ -1,13 +1,16 @@
 package io.github.thebroccolibob.bobsmobgear.mixin.client.bettercombat;
 
+import com.bawnorton.mixinsquared.TargetHandler;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.thebroccolibob.bobsmobgear.client.duck.TriggersAttack;
 import io.github.thebroccolibob.bobsmobgear.item.UsingAttackable;
 import net.bettercombat.api.WeaponAttributes;
 import net.bettercombat.logic.WeaponRegistry;
+import net.bettercombat.mixin.client.MinecraftClientInject;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,19 +21,19 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = MinecraftClient.class, priority = 1500)
 public class MinecraftClientInjectMixin implements TriggersAttack {
     @Shadow @Nullable public ClientPlayerEntity player;
-    /**
-     * @see net.bettercombat.mixin.client.MinecraftClientInject#startUpswing(WeaponAttributes)
-     */
+
+    @Dynamic(mixin = MinecraftClientInject.class)
     @Shadow
     private void startUpswing(WeaponAttributes attributes) {
         throw new AssertionError();
     }
 
-    /**
-     * @see net.bettercombat.mixin.client.MinecraftClientInject#startUpswing(WeaponAttributes)
-     */
+    @TargetHandler(
+            mixin = "net.bettercombat.mixin.client.MinecraftClientInject",
+            name = "startUpswing"
+    )
     @ModifyExpressionValue(
-            method = "startUpswing",
+            method = "@MixinSquared:Handler",
             remap = false,
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z")
     )
