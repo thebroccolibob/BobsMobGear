453314e17d57958a74ca0382341f03af85a216e7
ThePotatoArchivist <josiahfu@gmail.com>
2025-11-17 21:32:06 -0800

    Fix other datagen requirement

--- src/client/kotlin/io/github/thebroccolibob/bobsmobgear/datagen/EmiRecipeDefaultGenerator.kt
+++ src/client/kotlin/io/github/thebroccolibob/bobsmobgear/datagen/EmiRecipeDefaultGenerator.kt
@@ -1,11 +1,12 @@
 package io.github.thebroccolibob.bobsmobgear.datagen
 
-import com.mojang.serialization.Codec
 import io.github.thebroccolibob.bobsmobgear.BobsMobGear
+import io.github.thebroccolibob.bobsmobgear.BobsMobGearCompat
 import io.github.thebroccolibob.bobsmobgear.registry.BobsMobGearBlocks
 import io.github.thebroccolibob.bobsmobgear.registry.BobsMobGearFluids
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricCodecDataProvider
+import com.mojang.serialization.Codec
 import net.minecraft.data.DataOutput.OutputType
 import net.minecraft.registry.Registries
 import net.minecraft.registry.RegistryWrapper
@@ -32,7 +33,7 @@ class EmiRecipeDefaultGenerator(
 
         val RECIPES =
             RecipeGenerator.TOOL_TYPES.flatMap { it.getRecipeIds() } +
-            ReplacementRecipeGenerator.TOOL_TYPES.flatMap { it.getRecipeIds() } +
+            (if (BobsMobGearCompat.DATAGEN_REQUIREMENTS) ReplacementRecipeGenerator.TOOL_TYPES.flatMap { it.getRecipeIds() } else listOf()) +
             listOf(BobsMobGearFluids.IRON, BobsMobGearFluids.DIAMOND, BobsMobGearFluids.BLACK_STEEL).map {
                 Registries.FLUID.getId(it).withPrefixedPath("forging/")
             } +
